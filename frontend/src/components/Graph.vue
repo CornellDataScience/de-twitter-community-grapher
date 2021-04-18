1d05e4ceed3decc0cebbdf5cdfa37dfa2500ecbf
Arjun Nirav Shah <ans96@cornell.edu>
2021-04-18 15:14:30 -0400

    links work with nodes

--- frontend/src/components/Graph.vue
+++ frontend/src/components/Graph.vue
@@ -1,11 +1,9 @@
 <template>
 
   <div>
-    <h1>Test</h1>
+    <h1>{{user}}</h1>
 
-    <svg width='500' height='500' class='arjuns test'>
-
-    </svg>
+    <svg width='500' height='500' class='arjuns test'></svg>
 
   </div>
 
@@ -23,63 +21,60 @@ import { defineComponent } from "vue";
        
     },
     mounted() {
-    const svg = d3.select('svg')
-    const width = svg.attr('width')
-    const height = svg.attr('height')
+      const svg = d3.select('svg')
+      const width = svg.attr('width')
+      const height = svg.attr('height')
 
-    console.log(width);
-    console.log(height);
-
-    const nodes = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
-
-      const links = [
-          {source: 'node1', target: 'node2'},
-          {source: 'node1', target: 'node3'},
-          {source: 'node2', target: 'node3'},
+      const nodes = [{name: 0}, {name: 1}, {name: 2}, {name: 3}, {name: 4}, {name: 5}, {name: 6}, {name: 7}]
+      
+      const links = [ // uses index in node array
+        {source: 0, target: 1},
+        {source: 0, target: 2},
+        {source: 0, target: 3},
+        {source: 1, target: 6},
+        {source: 3, target: 4},
+        {source: 3, target: 7},
+        {source: 4, target: 5}, 
+        {source: 4, target: 7}
       ]
 
-      function ticked() {
-        const u = d3.select('svg')
-          .selectAll('circle')
-          .data(nodes)
-
-        u.enter()
-          .append('circle')
-          .attr('r', 5)
-          .merge(u)
-          .attr('cx', function(d) {
-            return d.x
-          })
-          .attr('cy', function(d) { 
-            return d.y
-          })
-
-        u.exit().remove()
-      }
-
-
       const simulation = d3.forceSimulation(nodes)
-  .force('charge', d3.forceManyBody())
-  .force('center', d3.forceCenter(width / 2, height / 2))
-  .on('tick', ticked);
+        .force('charge', d3.forceManyBody())
+        .force('center', d3.forceCenter(width / 2, height / 2))
+        .force('link',d3.forceLink(links).id(function(d) { return d.name;}))
+            
+        function ticked() {
+          const node = d3.select('svg')
+            .selectAll('circle')
+            .data(nodes)
 
-  simulation.force('charge', d3.forceManyBody())
+          const link = d3.select('svg')
+            .selectAll('line')
+            .data(links)
 
-  /*
-      function ticked(e) {
+          link.enter()
+            .append('line')
+            .merge(link)
+            .attr('class','link')
+            .attr('x1', function(d) { return d.source.x; })
+            .attr('y1', function(d) { return d.source.y; })
+            .attr('x2', function(d) { return d.target.x; })
+            .attr('y2', function(d) { return d.target.y; })
 
-        node.attr('cx', function(d) { return d.x; })
-          .attr('cy', function(d) { return d.y; })
-          .call(force.drag);
+          node.enter()
+            .append('circle')
+            .attr('class','node')
+            .attr('r', 5)
+            .merge(node)
+            .attr('cx', function(d) { return d.x; })
+            .attr('cy', function(d) { return d.y; })
 
-        link.attr('x1', function(d) { return d.source.x; })
-          .attr('y1', function(d) { return d.source.y; })
-          .attr('x2', function(d) { return d.target.x; })
-          .attr('y2', function(d) { return d.target.y; })
-      }
-      */
+          node.exit().remove()
 
-      
+        }
+
+      simulation.on('tick',ticked)
+
     },
     data: function() {
       return {
@@ -89,20 +84,19 @@ import { defineComponent } from "vue";
       
   });
 
-
 </script>
 
 <style>
   .node {
-    fill: #ccc;
-    stroke: #fff;
+    fill: rgb(255, 0, 0);
     stroke-width: 2px;
   }
 
   .link {
-    stroke: #777;
-    stroke-width: 2px;
+    stroke: rgb(0, 0, 0);
+    stroke-width: 1px;
   }
   
 </style>
+
 
